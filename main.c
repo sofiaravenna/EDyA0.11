697e86bdc556dc420e65d391b39cb65f0cfa255b
Sofía <ravennasofia03@gmail.com>
2023-05-03 20:15:58 -0300

    no funciona

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include<stdlib.h>
 
+void mAYUSCULA(char *);
 int main() {
-    printf("Hello, World!\n");
+    char texto[1000];
+    printf("Ingresar texto: ");
+    fflush(stdin);
+    gets(texto);
+    mAYUSCULA(texto);
+    printf("%s",texto);
     return 0;
+}
+
+void mAYUSCULA(char *string){
+    for (int i = 0; string[i]!='\0' ;++i) {
+        string[i]-=32;
+
+        if(string[i]==' '){
+            string[i+1]+=32;
+        }
+    }
+
 }
